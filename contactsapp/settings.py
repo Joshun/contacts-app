387fcec78d0b0f6b4b1d53e852af8c18cb32005e
Joshua O'Leary <jmoey139@gmail.com>
2025-02-18 19:23:23 +0000

    register app

--- contactsapp/settings.py
+++ contactsapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'contactscore.apps.ContactscoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
