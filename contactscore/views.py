074289f8bdd14cfb0cd5effdf2447da7cc5af107
Joshua O'Leary <jmoey139@gmail.com>
2025-02-18 19:45:10 +0000

    basic homepage

--- contactscore/views.py
+++ contactscore/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.urls import path
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, "contactscore/index.html")
+
+
+urlpatterns = [
+    path("", index),
+]
