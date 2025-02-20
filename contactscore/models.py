5a09a51a860d215cb0a2bdf518631f9d6fbe99c4
Joshua O'Leary <jmoey139@gmail.com>
2025-02-20 18:51:51 +0000

    use reversed urls

--- contactscore/models.py
+++ contactscore/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class ContactBook(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=255, null=True, blank=True)
+
+    @property
+    def view_url(self):
+        return reverse('view_contactbook_name', kwargs={"contact_book_id": self.pk})
+
+    @property
+    def add_contact_url(self):
+        return reverse('add_contact_name', kwargs={"contact_book_id": self.pk})
 
 
 
