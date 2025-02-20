860144a926bc9b43a68541e7198facb20fc7d5a0
Joshua O'Leary <jmoey139@gmail.com>
2025-02-20 20:26:51 +0000

    test you can only see your contacts

--- contactscore/tests.py
+++ contactscore/tests.py
@@ -10,6 +10,7 @@ class ContactsAppTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = User.objects.create(username='testuser', email='testuser@test.com')
+        cls.second_user = User.objects.create(username='testuser2', email='testuser2@test.com')
 
     def test_homepage_redirect_without_login(self):
         resp = self.client.get('/')
@@ -42,3 +43,20 @@ class ContactsAppTestCase(TestCase):
         resp = self.client.get(f'/view-contactbook/{contact_book.pk}')
         self.assertEqual(resp.status_code, 200)
         self.assertContains(resp, 'Ellie')
+
+    def test_can_only_see_your_contactbooks(self):
+        self.client.force_login(self.user)
+        contact_book = ContactBook.objects.create(name='TEST CONTACT BOOK USER 1', user=self.user)
+        contact_book2 = ContactBook.objects.create(name='TEST CONTACT BOOK USER 2', user=self.second_user)
+
+
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'TEST CONTACT BOOK USER 1')
+        self.assertNotContains(resp, 'TEST CONTACT BOOK USER 2')
+
+        self.client.force_login(self.second_user)
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'TEST CONTACT BOOK USER 2')
+        self.assertNotContains(resp, 'TEST CONTACT BOOK USER 1')
