5b1fe0f2e5f0c734367042e357d15562b13a0b98
Joshua O'Leary <jmoey139@gmail.com>
2025-02-20 19:53:59 +0000

    add some basic unit tests

--- contactscore/tests.py
+++ contactscore/tests.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
+from contactscore.models import ContactBook, Contact
+
+
 # Create your tests here.
+
+class ContactsAppTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(username='testuser', email='testuser@test.com')
+
+    def test_homepage_redirect_without_login(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 302)
+
+    def test_homepage_login(self):
+        self.client.force_login(self.user)
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, self.user.email)
+
+    def test_contactbook_present(self):
+        self.client.force_login(self.user)
+
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertNotContains(resp, 'TEST CONTACT BOOK')
+
+        contact_book = ContactBook.objects.create(name='TEST CONTACT BOOK', user=self.user)
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'TEST CONTACT BOOK')
+
+    def test_contact_present(self):
+        self.client.force_login(self.user)
+
+        contact_book = ContactBook.objects.create(name='TEST CONTACT BOOK', user=self.user)
+        contact = Contact.objects.create(contact_book=contact_book, name='Ellie')
+
+        resp = self.client.get(f'/view-contactbook/{contact_book.pk}')
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'Ellie')
