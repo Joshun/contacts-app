546aef92e926cc0127e21044bdadffce64e10cc5
Joshua O'Leary <jmoey139@gmail.com>
2025-02-23 21:40:49 +0000

    link up stat boxes to api

--- contactscore/static/contactscore/utils.js
+++ contactscore/static/contactscore/utils.js
@@ -1,5 +1,7 @@
 
-function loadHomepageTotals() {
+async function loadHomepageTotals() {
     // this is a demonstration of ajax - it could alternatively done using a template
+    let req = await fetch("/api/totals");
+    return await req.json();
 
 }
